b593f6790ffb6cfa0df4d375d536f1574a5ed5c6
sahan-onviro <sahan.onviro@gmail.com>
2024-02-07 14:58:04 +0545

    homepage

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('.destination-slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<img class="slick-prev" src="./assets/images/svg/arrow.svg">',
+        nextArrow: '<img class="slick-next" src="./assets/images/svg/arrow.svg">'
+    });
+    $('.testimonials-slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300, centerMode: true,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<img class="slick-prev" src="./assets/images/svg/arrow-2.svg">',
+        nextArrow: '<img class="slick-next" src="./assets/images/svg/arrow-2.svg">'
+    });
+});
